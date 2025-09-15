c393f53793b5aac7357031cca9b1f6913f802667
Jayprajapati19 <jayp37679@gmail.com>
2025-09-15 22:47:55 +0530

    new

--- js/vd7.js
+++ js/vd7.js
@@ -84,6 +84,30 @@
 
 
 // +++++++++++++++++++++++++
+// most optimised approach  left rotation
+// 47 min
+
+let arr = [1, 2, 3, 4, 5];
+let k = Number(prompt("Enter k value : "));
+
+k = k % arr.length;
+
+reverse(0, k - 1)
+reverse(k, arr.length - 1)
+reverse(0, arr.length - 1)
+console.log(arr);
+
+function reverse(i,j) {
+    while (i < j) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+}
 
 
-    
+
+
+
